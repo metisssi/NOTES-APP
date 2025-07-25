411a8d4b9a7dee551c3bcc780b6da1601ef693f4
metisssi <simcheraandrij@gmail.com>
2025-07-25 15:18:21 +0200

    change REDIRECT_URI

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,7 +8,7 @@
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: process.env.GOOGLE_CALLBACK_URL
+        callbackURL: process.env.REDIRECT_URI
     },
 
     async function(accessToken, refreshToken, profile, done) {
